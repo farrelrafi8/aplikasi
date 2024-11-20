6345b5c8be2e8f47edbc613cfe34e40cfd46b9ce
farrelrafi8 <farrelrafi2586@gmail.com>
2024-11-20 19:26:36 +0700

    personal

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-2">
+
+            <div class=" flex mt-6">
+                <h2 class="font-semibold text-xl">Add Product</h2>
+                <button class="bg-gray-100 px-10 py-2 rounded-md font-semibold">Tambah</button>
+            </div>
+
+            <div class="mt-4">
+                <form method="POST">
+
+                </form>
+
+            </div>
+
+        </div>
+</x-app-layout>
